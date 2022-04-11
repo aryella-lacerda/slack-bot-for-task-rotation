b90aebc6569e36307933e1c0f48fe0caf5eff5fb
Aryella Lacerda <aryella.lacerda@gmail.com>
2022-04-11 13:21:05 -0300

    chore: create database access methods for getting by task and deleting rotation

--- database/index.ts
+++ database/index.ts
@@ -1,3 +1,5 @@
 export * from "./put-rotation";
 export * from "./get-all-rotations";
 export * from "./get-rotations-by-channel-id";
+export * from "./delete-rotation";
+export * from "./get-rotation-by-task";
