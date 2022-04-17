1b28ef6b001e6d978c8614b37f380b95ffd02579
Aryella Lacerda <aryella.lacerda@gmail.com>
2022-04-17 10:21:01 -0300

    refactor: create RotationFrequency constant and constants folder

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '@database/?(.*)': '<rootDir>/src/database/$1',
     '@handlers/?(.*)': '<rootDir>/src/handlers/$1',
     '@entities/?(.*)': '<rootDir>/src/entities/$1',
+    '@constants/?(.*)': '<rootDir>/src/constants/$1',
     '@utils/?(.*)': '<rootDir>/src/utils/$1',
     '@tests/?(.*)': '<rootDir>/.jest/$1',
     '@seeds/?(.*)': '<rootDir>/.seeds/$1',
